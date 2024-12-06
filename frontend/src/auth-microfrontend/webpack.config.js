1f5da848d2034e6f829f6b5e292bd36cc7214fc8
Elena Popkova <elena.popkova@boostinternet.de>
2024-12-06 14:55:38 +0100

    microfrontends

---
+++ frontend/src/auth-microfrontend/webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    devServer: {
+        port: 3001,
+        historyApiFallback: true,
+    },
+    output: {
+        publicPath: "http://localhost:3001/",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: "babel-loader",
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: "auth-microfrontend",
+            filename: "remoteEntry.js",
+            exposes: {
+                "./Login": "/components/Login",
+                "./Register": "/components/Register",
+            },
+            remotes: {
+                core: "core-microfrontend@http://localhost:3003/remoteEntry.js",
+            },
+            shared: ["react", "react-dom"]
+        }),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+    ],
+};
